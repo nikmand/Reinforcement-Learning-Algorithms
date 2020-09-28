cb8438f4cf2f043d77e44c0cc2ee230f61d9e568
nikmand <nikiforosmandi@windowslive.com>
2020-09-28 12:18:49 +0300

    update setup file

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-with open('README.txt', 'r') as long_desc_file:
+with open('README.md', 'r') as long_desc_file:
     long_description = long_desc_file.read()
 
 setup(
@@ -9,9 +9,9 @@ setup(
     maintainer='Nikiforos Mandilaras',
     maintainer_email='nikmand@outlook.com',
     packages=['rl-library'],
-    url='',
+    url='https://github.com/nikmand/reinforcement-learning-library',
     license='',
-    description='',
+    description='Implementation of Reinforcement Learning Algorithms',
     long_description=long_description,
     classifiers=[
         'Programming Language :: Python :: 3 :: Only'
