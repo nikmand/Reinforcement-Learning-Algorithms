0e9c6be41d9327a3367e25ada2aa8eea3adda89a
nikmand <nikiforosmandi@windowslive.com>
2020-10-01 12:42:31 +0300

    renaming

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open('README.md', 'r') as long_desc_file:
     long_description = long_desc_file.read()
 
 setup(
-    name='rl-library',
+    name='rlsuite',
     version='1.0.0',
     maintainer='Nikiforos Mandilaras',
     maintainer_email='nikmand@outlook.com',
-    packages=['rl-library'],
+    packages=['rlsuite'],
     url='https://github.com/nikmand/reinforcement-learning-library',
     license='',
     description='Implementation of Reinforcement Learning Algorithms',
