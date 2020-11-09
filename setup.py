bd310516f573eb84a486706bf4e27279dd04d250
nikmand <nikiforosmandi@windowslive.com>
2020-11-09 16:44:20 +0200

    update dependencies

--- setup.py
+++ setup.py
@@ -15,13 +15,12 @@ setup(
     description='Implementation of Reinforcement Learning Algorithms',
     long_description=long_description,
     python_requires='>=3.5',
-    install_requires=[
-        "-f https://download.pytorch.org/whl/torch_stable.html",
-        "torch==1.5.0+cpu",
-        "torchvision==0.6.0+cpu",
-        "tb-nightly",
-        "gym",
-        "matplotlib"],
+    # install_requires=[
+    #     "torch==1.5.0+cpu",
+    #     "torchvision==0.6.0+cpu",
+    #     "tb-nightly",
+    #     "gym",
+    #     "matplotlib"],
     classifiers=[
         'Programming Language :: Python :: 3 :: Only'
     ]
