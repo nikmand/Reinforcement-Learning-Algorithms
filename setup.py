db968ce240cffcf3e20d2f02a7334aaa397d38ba
nikmand <nikiforosmandi@windowslive.com>
2020-11-09 23:16:57 +0200

    update dependencies

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version='1.0.0',
     maintainer='Nikiforos Mandilaras',
     maintainer_email='nikmand@outlook.com',
-    packages=find_namespace_packages(),
+    packages=find_namespace_packages(include=['rlsuite.*']),
     url='https://github.com/nikmand/reinforcement-learning-library',
     license='',
     description='Implementation of Reinforcement Learning Algorithms',
