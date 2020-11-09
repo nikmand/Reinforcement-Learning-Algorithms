158f2f7e839c19dee663dfca85452b3b0d1ff661
nikmand <nikiforosmandi@windowslive.com>
2020-11-09 16:36:07 +0200

    update dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup
 
 with open('README.md', 'r') as long_desc_file:
     long_description = long_desc_file.read()
@@ -13,6 +14,13 @@ setup(
     license='',
     description='Implementation of Reinforcement Learning Algorithms',
     long_description=long_description,
+    python_requires='>=3.5',
+    install_requires=[
+        "torch==1.5.0+cpu",
+        "torchvision==0.6.0+cpu",
+        "tb-nightly",
+        "gym",
+        "matplotlib"],
     classifiers=[
         'Programming Language :: Python :: 3 :: Only'
     ]
