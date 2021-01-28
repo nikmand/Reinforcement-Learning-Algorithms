c52e0d10f363bdaefda35bde6019024581acf251
ΣΤΡ(ΕΠ) Μανδηλαράς Νικηφόρος <nikmand@outlook.com>
2021-01-28 15:20:21 +0200

    minor changes

--- rlsuite/agents/classic_agents/mc_agent.py
+++ rlsuite/agents/classic_agents/mc_agent.py
@@ -5,8 +5,8 @@ from rlsuite.agents.agent import Agent
 
 
 class MCAgent(Agent):
-    """ Monte carlo without exploration start. This means that we don't have the option to start the simulation
-     from a different point every time but instead we explore the action space by using e-greedy"""
+    """ Implementation of Monte carlo without exploration start. The simulation starts from the same point every time
+     and we explore the action space by using e-greedy. """
 
     def __init__(self, num_of_actions, dimensions, gamma=0.999, epsilon=1):
         """
@@ -15,8 +15,8 @@ class MCAgent(Agent):
         self.num_of_actions = num_of_actions
         # q_table has the dimensions of each variable (state) and an extra one for every possible action from each state
         self.q_table = np.zeros(dimensions + [num_of_actions])
-        self.num_of_visits = np.zeros(dimensions + [num_of_actions])  # we keep the number of times that we have visited
-        # each Q(s, a)
+        # we keep the number of times that we have visited each Q(s, a)
+        self.num_of_visits = np.zeros(dimensions + [num_of_actions])
 
     def choose_action(self, cur_state, train=True):
         """"""
@@ -26,9 +26,9 @@ class MCAgent(Agent):
             return np.argmax(self.q_table[cur_state])
 
     def update(self, state_action_ls, rewards):
-        """ Method that runs at the end of every episode, we take into consideration only the first time that we
+        """ This method runs at the end of every episode. We take into consideration only the first time that we
         encounter a state. For each (state, action) we have the corresponding value. We update the mean Q(s, a) by
-        taking into consideration the value of the current episode."""
+        taking into consideration the value of the current episode. """
 
         visited_states = set()
         for (state, action), value in zip(state_action_ls, rewards):
