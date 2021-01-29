b46f7897264f72101e2ae8de2c6890dc06ce6e17
ΣΤΡ(ΕΠ) Μανδηλαράς Νικηφόρος <nikmand@outlook.com>
2021-01-29 17:48:25 +0200

    remove methods and fields that refer to specific implementations

--- rlsuite/agents/agent.py
+++ rlsuite/agents/agent.py
@@ -9,22 +9,9 @@ class Agent(ABC):
         self.num_of_actions = num_of_actions  # TODO remove this it can be deduced.
         self.gamma = gamma  # usually 0.8 - 0.99
         self.epsilon = epsilon
-        self.ada_divisor = 25  # np.prod(self.q_table.shape[:-1])
-
-    def adjust_exploration(self, i_episode):
-        self.epsilon = max(0.01, min(0.3, 1.0 - math.log10((i_episode + 1) / self.ada_divisor)))
-        #  /= np.sqrt(i_episode + 1)
 
     @abstractmethod
     def choose_action(self, cur_state):
-        raise NotImplementedError
-
-    @abstractmethod
-    def save_checkpoint(self, filename):
-        raise NotImplementedError
-
-    @abstractmethod
-    def load_checkpoint(self, filename):
         raise NotImplementedError
 
     @abstractmethod
